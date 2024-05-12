504f0101752591e8912a4dae12a0cb4bf3f0b5a4
mozammel <mozammel169649@gmail.com>
2024-05-12 10:39:56 +0600

    task-groupuser-tasks complete api

---
+++ src/modules/todo_management/tasks_group_users/models/tasks_group_users_model.ts
@@ -0,0 +1,97 @@
+import {
+    // Association,
+    DataTypes,
+    // HasManyAddAssociationMixin,
+    // HasManyCountAssociationsMixin,
+    // HasManyCreateAssociationMixin,
+    // HasManyGetAssociationsMixin,
+    // HasManyHasAssociationMixin,
+    // HasManySetAssociationsMixin,
+    // HasManyAddAssociationsMixin,
+    // HasManyHasAssociationsMixin,
+    // HasManyRemoveAssociationMixin,
+    // HasManyRemoveAssociationsMixin,
+    Model,
+    // ModelDefined,
+    // Optional,
+    Sequelize,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    DefaultSetOptions,
+    // NonAttribute,
+    // ForeignKey,
+} from 'sequelize';
+// enum is_complete {
+//     pending = 'pending',
+//     running = 'running',
+//     completed = 'completed',
+//     nexttime = 'nexttime',
+// }
+
+const tableName = 'tasks_group_users';
+const modelName = 'TasksGroupUsers';
+
+type Infer = InferAttributes<DataModel>;
+type InferCreation = InferCreationAttributes<DataModel>;
+
+class DataModel extends Model<Infer, InferCreation> {
+    declare id?: CreationOptional<number>;
+
+    declare branch_id: number;
+    declare user_id: number;
+    declare task_group_id: number;
+   
+    declare creator?: number;
+    declare status?: number;
+    declare created_at?: CreationOptional<Date>;
+    declare updated_at?: CreationOptional<Date>;
+}
+
+function init(sequelize: Sequelize) {
+    DataModel.init(
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            user_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: false,
+            },
+            task_group_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: false,
+            },
+            branch_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: false,
+            },
+           
+
+            creator: {
+                type: new DataTypes.TINYINT(),
+                allowNull: true,
+                defaultValue: null,
+            },
+            status: {
+                type: new DataTypes.TINYINT(),
+                allowNull: true,
+                defaultValue: 1,
+            },
+            created_at: DataTypes.DATE,
+            updated_at: DataTypes.DATE,
+        },
+        {
+            tableName: tableName,
+            modelName: modelName,
+            sequelize, // passing the `sequelize` instance is required
+            underscored: true,
+        },
+    );
+
+    return DataModel;
+}
+
+export { init, DataModel };
---
+++ src/modules/todo_management/tasks_group_users/services/update.ts
@@ -0,0 +1,85 @@
+import db from '../models/db';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { body, validationResult } from 'express-validator';
+import {
+    anyObject,
+    responseObject,
+    Request,
+} from '../../../common_types/object';
+import response from '../helpers/response';
+import { InferCreationAttributes } from 'sequelize';
+
+async function validate(req: Request) {
+    await body('id')
+        .not()
+        .isEmpty()
+        .withMessage('the id field is required')
+        .run(req);
+
+        await body('branch_id')
+        .not()
+        .isEmpty()
+        .withMessage('the branch_id field is required')
+        .run(req);
+
+    await body('user_id')
+        .not()
+        .isEmpty()
+        .withMessage('the task_id field is required')
+        .run(req);
+        
+    await body('task_group_id')
+        .not()
+        .isEmpty()
+        .withMessage('the task_group_id field is required')
+        .run(req);
+
+
+    let result = await validationResult(req);
+
+    return result;
+}
+
+async function update(
+    fastify_instance: FastifyInstance,
+    req: FastifyRequest,
+): Promise<responseObject> {
+    /** validation */
+    let validate_result = await validate(req as Request);
+    if (!validate_result.isEmpty()) {
+        return response(422, 'validation error', validate_result.array());
+    }
+
+    /** initializations */
+    let models = await db();
+    let body = req.body as anyObject;
+    let model = new models.TasksGroupUsersModel();
+
+    let inputs: InferCreationAttributes<typeof model> = {
+        branch_id: body.branch_id,
+        user_id: body.user_id,
+        task_group_id: body.task_group_id, 
+    };
+
+    /** print request data into console */
+    // console.clear();
+    // (fastify_instance as any).print(inputs);
+
+    /** store data into database */
+    try {
+        let data = await models.TasksGroupUsersModel.findByPk(body.id);
+        if (data) {
+            data.update(inputs);
+            await data.save();
+            return response(200, 'data updated', data);
+        } else {
+            return response(500, 'data update failed', {
+                error: 'data not found by given query',
+            });
+        }
+    } catch (error) {
+        return response(500, 'data creation error', { error });
+    }
+}
+
+export default update;
