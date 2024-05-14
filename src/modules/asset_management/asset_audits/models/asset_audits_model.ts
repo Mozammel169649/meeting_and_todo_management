fdbe3bd77e81caee2e6e9c11ebf0b6f38e3f260a
mozammel <mozammel169649@gmail.com>
2024-05-14 20:02:54 +0600

    asset_audits API

---
+++ src/modules/asset_management/asset_audits/models/asset_audits_model.ts
@@ -0,0 +1,95 @@
+import {
+    // Association,
+    DataTypes,
+    // HasManyAddAssociationMixin,
+    // HasManyCountAssociationsMixin,
+    // HasManyCreateAssociationMixin,
+    // HasManyGetAssociationsMixin,
+    // HasManyHasAssociationMixin,
+    // HasManySetAssociationsMixin,
+    // HasManyAddAssociationsMixin,
+    // HasManyHasAssociationsMixin,
+    // HasManyRemoveAssociationMixin,
+    // HasManyRemoveAssociationsMixin,
+    Model,
+    // ModelDefined,
+    // Optional,
+    Sequelize,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    DefaultSetOptions,
+    // NonAttribute,
+    // ForeignKey,
+} from 'sequelize';
+
+const tableName = 'asset_audits';
+const modelName = 'AssetAuditsModel';
+
+type Infer = InferAttributes<DataModel>;
+type InferCreation = InferCreationAttributes<DataModel>;
+type status = 'active' | 'deactive';
+
+class DataModel extends Model<Infer, InferCreation> {
+    declare id?: CreationOptional<number>;
+
+    declare branch_id: number;
+    declare title: string;
+    declare date: string;
+    declare description: string;
+
+    declare status?: status;
+    declare creator?: number;
+    declare created_at?: CreationOptional<Date>;
+    declare updated_at?: CreationOptional<Date>;
+}
+
+function init(sequelize: Sequelize) {
+    DataModel.init(
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            branch_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: false,
+            },
+            title: {
+                type: new DataTypes.STRING(60),
+                allowNull: false,
+            },
+            date: {
+                type: new DataTypes.STRING(30),
+                allowNull: false,
+            },
+            description: {
+                type: new DataTypes.TEXT(),
+                allowNull: false,
+            },
+
+            creator: {
+                type: new DataTypes.TINYINT(),
+                allowNull: true,
+                defaultValue: null,
+            },
+            status: {
+                type: new DataTypes.ENUM('active', 'deactive'),
+                defaultValue: 'active',
+            },
+            created_at: DataTypes.DATE,
+            updated_at: DataTypes.DATE,
+        },
+        {
+            tableName: tableName,
+            modelName: modelName,
+            sequelize, // passing the `sequelize` instance is required
+            underscored: true,
+        },
+    );
+
+    return DataModel;
+}
+
+export { init, DataModel };
