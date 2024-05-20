d2af4f6d47273b945118f06e6f7a20d83a241fbb
mozammel <mozammel169649@gmail.com>
2024-05-20 10:45:03 +0600

    notice_categories API

---
+++ src/modules/notice_management/notice_categories/services/store.ts
@@ -0,0 +1,70 @@
+import db from '../models/db';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { body, validationResult } from 'express-validator';
+import {
+    anyObject,
+    responseObject,
+    Request,
+} from '../../../common_types/object';
+import response from '../helpers/response';
+import { InferCreationAttributes } from 'sequelize';
+
+async function validate(req: Request) {
+    await body('branch_id')
+        .not()
+        .isEmpty()
+        .withMessage('the branch_id field is required')
+        .run(req);
+
+    await body('title')
+        .not()
+        .isEmpty()
+        .withMessage('the title field is required')
+        .run(req);
+
+    await body('description')
+        .not()
+        .isEmpty()
+        .withMessage('the description field is required')
+        .run(req);
+
+    let result = await validationResult(req);
+
+    return result;
+}
+
+async function store(
+    fastify_instance: FastifyInstance,
+    req: FastifyRequest,
+): Promise<responseObject> {
+    /** validation */
+    let validate_result = await validate(req as Request);
+    if (!validate_result.isEmpty()) {
+        return response(422, 'validation error', validate_result.array());
+    }
+
+    /** initializations */
+    let models = await db();
+    let body = req.body as anyObject;
+    let data = new models.NoticeCategorysModel();
+
+    let inputs: InferCreationAttributes<typeof data> = {
+        branch_id: body.branch_id,
+        title: body.title,
+        description: body.description,
+    };
+
+    /** print request data into console */
+    // console.clear();
+    // (fastify_instance as any).print(inputs);
+
+    /** store data into database */
+    try {
+        (await data.update(inputs)).save();
+        return response(200, 'data created', data);
+    } catch (error) {
+        return response(500, 'data creation error', { error });
+    }
+}
+
+export default store;
